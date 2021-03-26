a9e96137fb7329772381aebaa2aaf57e0a45450e
vgdsilva <gasparellovinicius@gmail.com>
2021-03-26 11:19:12 -0300

    Exercicios

---
+++ Secao4Curso/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Seção4Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class AreaTriangulo
+    {
+        public void calcularArea()
+        {
+
+        }
+    }
+}
